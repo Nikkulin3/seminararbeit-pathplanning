bbe9be5932b67907f457b3560309acb518a936f2
Julin Nowoczyn <nik.nowoczyn@student.uni-augsburg.de>
2022-11-17 15:03:47 +0100

    windows support

---
+++ compile_latex.bat
@@ -0,0 +1,6 @@
+pdflatex -interaction=nonstopmode 000_Main.tex
+biber 000_Main
+pdflatex -interaction=nonstopmode 000_Main.tex
+biber 000_Main
+pdflatex -interaction=nonstopmode 000_Main.tex
+pdflatex -interaction=nonstopmode 000_Main.tex
